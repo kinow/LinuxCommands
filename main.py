12801a033059b3a812745ed181fd678e7fbb0658
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-07-03 23:24:35 +1200

    Interface to browser via telnet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys;
 import os;
-from StringIO import StringIO
 
 #speech
 from SpeakPython.SpeakPythonRecognizer import SpeakPythonRecognizer
@@ -9,10 +8,9 @@ from SpeakPython.SpeakPythonRecognizer import SpeakPythonRecognizer
 from subprocess import call
 
 #browser control
-from PyQt4.QtCore import Qt
-import spynner
+import telnetlib
 
-browser = None
+tn = telnetlib.Telnet("localhost", 32000)
 
 def execute(s):
 	exec(s)
@@ -30,13 +28,8 @@ def weather(s):
 	print("Calling NIWA API for " + s)
 	url = makeUrl(s)
 	print("URL: " + url)
-	#browser.show()
-	browser.load(url=u""+url, load_timeout=30000, tries=1)
+	tn.write("window.location='"+url+"'" + "\n")
 
-debug_stream = StringIO()
-browser = spynner.Browser(debug_level=spynner.DEBUG, debug_stream=debug_stream)
-browser.show()
-browser.load(url="http://weather.niwa.co.nz/auckland", load_timeout=30000, tries=1)
 try:
 	recog = SpeakPythonRecognizer(execute, "linux")
 	recog.setDebug(1);
